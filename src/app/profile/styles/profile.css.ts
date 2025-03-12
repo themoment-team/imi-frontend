6f90d586715473def5800464d5090d6923181a1e
h-0y28 <s24015@gsm.hs.kr>
2025-03-13 08:44:32 +0900

    style: add common styles for profile pages

---
+++ src/app/profile/styles/profile.css.ts
@@ -0,0 +1,24 @@
+import { style } from '@vanilla-extract/css';
+
+import { vars } from '@/styles/theme.css';
+
+export const TextContainer = style({
+  display: 'flex',
+  gap: '0.3rem',
+  borderBottom: 'solid 2px',
+  borderColor: vars.color.primary,
+  padding: '0.5rem 0',
+  fontSize: '1.25rem',
+  // backgroundColor: vars.color.white,
+  backgroundColor: 'transparent',
+});
+
+export const Tag = style({
+  backgroundColor: vars.color.primary,
+  width: 'fit-content',
+  padding: '0 0.1rem',
+});
+
+export const Content = style({
+  backgroundColor: 'transparent',
+});
