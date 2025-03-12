142418571cd9ab3627fc9ec4f8105c8bbfe5af9a
h-0y28 <s24015@gsm.hs.kr>
2025-03-13 08:43:26 +0900

    style: : publish the profile detail page

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import EmptyProfile from '@/components/EmptyProfile/EmptyProfile';
+
+import BackBtn from '../../../asset/BackBtn.png';
+import * as T from '../styles/profile.css';
+import * as S from './profile.css';
+
+const profile = {
+  id: 1111,
+  name: '정효주',
+  major: 'FE',
+  wanted: ['더모먼트', 'MSG'],
+  content:
+    '안녕하세요 효주예요 제 엠비티아이는 엔프제이에요 친구들이 자꾸 저보고 티라고 하는데 저는 에프에요 사실 이 학교오고 티가 되긴 했는데 에프라고 우기고 있어요 감사합니다. 안녕하세요 효주예요 제 엠비티아이는 엔프제이에요 친구들이 자꾸 저보고 티라고 하는데 저는 에프에요 사실 이 학교오고 티가 되긴 했는데 에프라고 우기고 있어요 감사합니다. 안녕하세요 효주예요 제 엠비티아이는 엔프제이에요 친구들이 자꾸 저보고 티라고 하는데 저는 에프에요 사실 이 학교오고 티가 되긴 했는데 에프라고 우기고 있어요 감사합니다. 안녕하세요 효주예요 제 엠비티아이는 엔프제이에요 친구들이 자꾸 저보고 티라고 하는데 저는 에프에요 사실 이 학교오고 티가 되긴 했는데 에프라고 우기고 있어요 감사합니다. 안녕하세요 효주예요 제 엠비티아이는 엔프제이에요 친구들이 자꾸 저보고 티라고 하는데 저는 에프에요 사실 이 학교오고 티가 되긴 했는데 에프라고 우기고 있어요 감사합니다.  안녕하세요 효주예요 제 엠비티아이는 엔프제이에요 친구들이 자꾸 저보고 티라고 하는데 저는 에프에요 사실 이 학교오고 티가 되긴 했는데 에프라고 우기고 있어요 감사합니다. 안녕하세요 효주예요 제 엠비티아이는 엔프제이에요 친구들이 자꾸 저보고 티라고 하는데 저는 에프에요 사실 이 학교오고 티가 되긴 했는데 에프라고 우기고 있어요 감사합니다.',
+};
+
+export default function ProfileDetail() {
+  const router = useRouter();
+
+  return (
+    <div>
+      {profile ? (
+        <div className={S.ProfileDetailContainer}>
+          <div className={S.BackBtn} onClick={() => router.back()}>
+            <Image src={BackBtn} alt="Back" width={16} height={30} />
+          </div>
+          <h1 className={S.Name}>{profile.name}</h1>
+          <div className={S.ContentContainer}>
+            <a
+              className={S.EditBtn}
+              onClick={() => router.push(`/write/${profile.id}`)}
+            >
+              수정하기
+            </a>
+            <div className={T.TextContainer}>
+              <p className={T.Tag}>관심 분야 </p>
+              <p className={T.Content}>{profile.major}</p>
+            </div>
+            <div className={T.TextContainer}>
+              <p className={T.Tag}>희망 동아리 </p>
+              <p className={T.Content}>{profile.wanted}</p>
+            </div>
+
+            <p className={S.Content}>{profile.content}</p>
+          </div>
+        </div>
+      ) : (
+        <EmptyProfile />
+      )}
+    </div>
+  );
+}
---
+++ src/app/profile/[id]/profile.css.ts
@@ -0,0 +1,40 @@
+import { style } from '@vanilla-extract/css';
+
+import { vars } from '@/styles/theme.css';
+
+export const ProfileDetailContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '6.5rem 20rem',
+});
+
+export const BackBtn = style({
+  cursor: 'pointer',
+});
+
+export const Name = style({
+  fontSize: '2rem',
+  paddingBottom: '5.5rem',
+  textAlign: 'center',
+});
+
+export const EditBtn = style({
+  alignSelf: 'flex-end',
+  padding: '0.4rem 0.9rem',
+  border: 'solid 1px',
+  borderColor: vars.color.primary,
+  borderRadius: '1rem',
+  width: 'fit-content',
+  cursor: 'pointer',
+});
+
+export const ContentContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '0 4.5rem',
+});
+
+export const Content = style({
+  fontSize: '1.25rem',
+  lineHeight: '2.5rem',
+});
