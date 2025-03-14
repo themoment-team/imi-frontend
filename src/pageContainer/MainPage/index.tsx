ba991bbe5a0bf3b6e30cbaa4c18914f577c7f2d3
junjuny0227 <junjuny0227@gmail.com>
2025-03-14 14:44:01 +0900

    refactor: 폴더 구조 변경

---
+++ src/pageContainer/MainPage/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './main.css';
+
+const MainPage = () => {
+  return <div>Main</div>;
+};
+
+export default MainPage;
---
+++ src/pageContainer/ProfilePage/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './profile.css';
+
+const ProfilePage = () => {
+  return <div>Profile</div>;
+};
+
+export default ProfilePage;
