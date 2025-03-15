b2c2cb59e4157388188eb5cc0f261cd0e8058932
junjuny0227 <junjuny0227@gmail.com>
2025-03-15 16:55:15 +0900

    feat: clubs 페이지 제작

--- src/pageContainer/ClubsPage/index.tsx
+++ src/pageContainer/ClubsPage/index.tsx
@@ -4,8 +4,12 @@ import * as S from './clubs.css';
 
 const ClubsPage = () => {
   return (
-    <div>
-      <ClubCard />
+    <div className={S.ClubsPageContainer}>
+      <div className={S.HeaderRow}>
+        <div className={S.AccentBar} />
+        <p className={S.PageTitle}>동아리 목록</p>
+      </div>
+      <div className={S.ClubCardsContainer}></div>
     </div>
   );
 };
