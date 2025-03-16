f2d2a81478d3479e57684c5c344f3bdaa99576e6
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-03-16 22:48:28 +0900

    feact: 로그인 페이지 퍼블리싱 완성

--- src/pageContainer/SignInPage/index.tsx
+++ src/pageContainer/SignInPage/index.tsx
@@ -1,7 +1,81 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { CloseEyes, ImiLogo, OpenEyes } from '@/asset';
+
+import { useState } from 'react';
+
 import * as S from './signIn.css';
 
 const SignInPage = () => {
-  return <div>SignIn</div>;
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  const [emailValue, setEmailValue] = useState<string>('');
+  const [pwValue, setPwValue] = useState<string>('');
+
+  const router = useRouter();
+
+  const handleRouter = () => {
+    router.push('/signup');
+  };
+
+  const handlePassword = () => {
+    setOpen((value) => !value);
+  };
+
+  return (
+    <div className={S.SigninContainer}>
+      <div className={S.LogoContainer}>
+        <ImiLogo width="4.125rem" height="3rem" />
+      </div>
+      <div className={S.InputContainer}>
+        <div className={S.InputEmailContainer}>
+          <p className={S.Text}>Email</p>
+          <div className={S.InputEmail}>
+            <input
+              placeholder="이메일을 입력해주세요"
+              className={S.InputBox}
+              onChange={(e) => setEmailValue(e.target.value)}
+            />
+            <p className={S.InputText}>@gsm.hs.kr</p>
+          </div>
+        </div>
+        <div className={S.InputEmailContainer}>
+          <p className={S.Text}>Password</p>
+          <div className={S.InputEmail}>
+            {isOpen ? (
+              <input
+                type="text"
+                placeholder="비밀번호를 입력해주세요"
+                className={S.InputBox}
+                onChange={(e) => setPwValue(e.target.value)}
+                value={pwValue}
+              />
+            ) : (
+              <input
+                type="password"
+                placeholder="비밀번호를 입력해주세요"
+                className={S.InputBox}
+                onChange={(e) => setPwValue(e.target.value)}
+                value={pwValue}
+              />
+            )}
+            <div className={S.IconBox} onClick={handlePassword}>
+              {isOpen ? <OpenEyes /> : <CloseEyes />}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={S.LoginBtn}>로그인</div>
+      <div className={S.SignupBox}>
+        <p className={S.Text}>계정이 없으신가요?</p>
+        <p className={S.GrayText} onClick={handleRouter}>
+          회원가입
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignInPage;
--- src/pageContainer/SignInPage/signIn.css.ts
+++ src/pageContainer/SignInPage/signIn.css.ts
@@ -1,1 +1,127 @@
-2;
+import { style } from '@vanilla-extract/css';
+
+import { vars } from '@/styles/theme.css';
+
+export const SigninContainer = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  width: '100vw',
+  height: '100vh',
+  background: vars.color.background,
+});
+
+export const LogoContainer = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '9.375rem',
+  height: '9.375rem',
+  background: vars.color.white,
+  borderRadius: '50%',
+});
+
+export const InputContainer = style({
+  display: 'flex',
+  width: '30rem',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '1rem',
+});
+
+export const InputEmailContainer = style({
+  display: 'flex',
+  width: '30.125rem',
+  padding: '1rem 1rem 1rem 0',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'start',
+});
+
+export const InputEmail = style({
+  display: 'flex',
+  alignItems: 'center',
+  border: `2px solid ${vars.color.primary} `,
+  borderRadius: '8px',
+  justifyContent: 'space-between',
+  padding: '1rem',
+  width: '30.125rem',
+  height: '4.5625rem',
+});
+
+export const InputBox = style({
+  border: 'none',
+  color: 'black',
+  width: '100%',
+  background: 'none',
+  selectors: {
+    '&:placehorder': {
+      color: vars.color.gray,
+    },
+    '&:focus': {
+      outline: 'none',
+      border: 'none',
+    },
+  },
+});
+
+export const InputPW = style({
+  display: 'flex',
+  width: '30rem',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '1rem',
+});
+
+export const Text = style({
+  color: ' #1E1E1E',
+  textAlign: 'center',
+  fontSize: '1rem',
+});
+
+export const SignoutText = style({
+  color: ' #1E1E1E',
+  textAlign: 'center',
+  fontSize: '1rem',
+});
+
+export const GrayText = style({
+  color: vars.color.gray,
+  textAlign: 'center',
+  fontSize: '1rem',
+  marginLeft: '0.5rem',
+  cursor: 'pointer',
+});
+
+export const InputText = style({
+  color: vars.color.gray,
+  background: 'none',
+});
+
+export const IconBox = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const LoginBtn = style({
+  width: '30rem',
+  height: '2.4374rem',
+  background: vars.color.primary,
+  borderRadius: '0.5rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginTop: '2.94rem',
+});
+
+export const SignupBox = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'row',
+  width: '15.6rem',
+  height: '1.375rem',
+  marginTop: '1.5rem',
+});
