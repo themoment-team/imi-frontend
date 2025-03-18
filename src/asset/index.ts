fda967825713a7aad65384913faeae14de8936d7
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-03-18 23:12:16 +0900

    fix: svg 추가

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,1 +1,4 @@
 export { default as ImiLogo } from './ImiLogo';
+export { default as Github } from './Github';
+export { default as Insta } from './Insta';
+export { default as Discord } from './Discord';
