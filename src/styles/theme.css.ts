a172ecf29f0da9990e17768f9bf86350473b5db3
h-0y28 <s24015@gsm.hs.kr>
2025-03-20 17:01:25 +0900

    style: white color 수정

--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -7,6 +7,6 @@ export const [themeClass, vars] = createTheme({
     background: '#F5F7FB',
     text: '#1C1C1E', // black
     gray: '#A0A3A8',
-    white: '#FFFFFF',
+    white: '#FAFAFA',
   },
 });
