edd8d611a6b52f60c57e207dfa307f41dcb7614a
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-03-24 17:17:59 +0900

    feat: 새로운 페이지 import

--- src/pageContainer/index.ts
+++ src/pageContainer/index.ts
@@ -3,5 +3,5 @@ export { default as ClubsPage } from './ClubsPage';
 export { default as MainPage } from './MainPage';
 export { default as NotFoundPage } from './NotFoundPage';
 export { default as SignInPage } from './SignInPage';
-export { default as SignUpPage } from './SignUpPage/stepOne';
+export { default as SignUpPage } from './SignUpPage';
 export { default as WritePage } from './WritePage';
