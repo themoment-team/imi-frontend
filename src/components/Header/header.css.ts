54df62db2e4c3394fbcc07e36b0f0975fdd0cef9
h-0y28 <s24015@gsm.hs.kr>
2025-03-25 00:28:35 +0900

    style: 반응형 추가

--- src/components/Header/header.css.ts
+++ src/components/Header/header.css.ts
@@ -5,6 +5,33 @@ import { vars } from '@/styles/theme.css';
 export const HeaderBox = style({
   padding: '1rem 17.5rem',
   background: vars.color.white,
+
+  '@media': {
+    'screen and (max-width: 1728px)': {
+      padding: '1rem 15rem',
+    },
+    'screen and (max-width: 1600px)': {
+      padding: '1rem 13rem',
+    },
+    'screen and (max-width: 1440px)': {
+      padding: '1rem 11rem',
+    },
+    'screen and (max-width: 1200px)': {
+      padding: '1rem 8rem',
+    },
+    'screen and (max-width: 1024px)': {
+      padding: '1rem 5rem',
+    },
+    'screen and (max-width: 768px)': {
+      padding: '1rem 3rem',
+    },
+    'screen and (max-width: 480px)': {
+      padding: '1rem 0.7rem',
+    },
+    'screen and (max-width: 375px)': {
+      padding: '1rem 1.5rem',
+    },
+  },
 });
 
 export const HeaderWrapper = style({
@@ -16,16 +43,44 @@ export const HeaderWrapper = style({
 export const RightItems = style({
   display: 'flex',
   gap: '4rem',
+  '@media': {
+    'screen and (max-width: 1200px)': {
+      gap: '3.5rem',
+    },
+    'screen and (max-width: 1024px)': {
+      gap: '3rem',
+    },
+    'screen and (max-width: 768px)': {
+      gap: '2.5rem',
+    },
+    'screen and (max-width: 480px)': {
+      gap: '1rem',
+    },
+    'screen and (max-width: 375px)': {
+      gap: '1.5rem',
+    },
+  },
 });
 
 export const LogoBox = style({
   cursor: 'pointer',
+  alignContent: 'center',
 });
 
 export const Navigation = style({
   display: 'flex',
   alignItems: 'center',
   gap: '0.8125rem',
+  '@media': {
+    'screen and (max-width: 480px)': {
+      gap: '0.5rem',
+    },
+    'screen and (max-width: 375px)': {
+      flexDirection: 'column',
+      alignItems: 'start',
+      gap: '0.3rem',
+    },
+  },
 });
 
 export const NavigationBtn = style({
