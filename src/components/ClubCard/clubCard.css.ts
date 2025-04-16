6bb4c39eb63571f556c7c4695481644777eb9f6b
junjuny0227 <junjuny0227@gmail.com>
2025-04-16 23:24:51 +0900

    style: 작은 스타일 변경

--- src/components/ClubCard/clubCard.css.ts
+++ src/components/ClubCard/clubCard.css.ts
@@ -3,8 +3,7 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '@/styles/theme.css';
 
 export const CardContainer = style({
-  maxWidth: '22.5rem',
-  height: '18.75rem',
+  width: '22.5rem',
   backgroundColor: vars.color.background,
   borderRadius: '1rem',
   display: 'flex',
@@ -17,18 +16,18 @@ export const CardContainer = style({
 export const ImageWrapper = style({
   position: 'relative',
   width: '100%',
-  height: '12.9375rem',
+  height: '12.875rem',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
 });
 
 export const InfoContainer = style({
-  padding: '1rem 1rem 1.65rem 1rem',
+  padding: '0.875rem 1rem 1.1rem 1rem',
   display: 'flex',
+  gap: '0.5rem',
   flexDirection: 'column',
   flex: 1,
-  justifyContent: 'space-between',
   borderTop: `1px solid ${vars.color.primary}`,
   color: vars.color.text,
 });
@@ -36,11 +35,13 @@ export const InfoContainer = style({
 export const ClubTitle = style({
   fontSize: '1.5rem',
   fontWeight: 500,
+  margin: '0.1563rem 0',
 });
 
 export const ProjectTitle = style({
   fontSize: '1rem',
   fontWeight: 300,
+  margin: '0.0938rem 0 0.0938rem 0.0625rem',
 });
 
 export const LeaderBadge = style({
