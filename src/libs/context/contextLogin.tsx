ac0e3406e34abb9c2e9121829f28f7190b42a85a
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-04-22 22:52:02 +0900

    feat:로그인 여부 provider 제작

---
+++ src/libs/context/contextLogin.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, createContext, useContext, useState } from 'react';
+
+type AuthContextType = {
+  isLogged: boolean;
+  setIsLogged: (value: boolean) => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  return context;
+};
+
+export const LoginProvider = ({ children }: { children: ReactNode }) => {
+  const [isLogged, setIsLogged] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isLogged, setIsLogged }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
