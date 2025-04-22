cb611d45ea0a44ab20e7b0dfe0986b805a7cc6e7
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-04-22 23:21:25 +0900

    feat:boolean타입 -> null, 예외처리 추가

--- src/libs/context/contextLogin.tsx
+++ src/libs/context/contextLogin.tsx
@@ -5,10 +5,13 @@ type AuthContextType = {
   setIsLogged: (value: boolean) => void;
 };
 
-const AuthContext = createContext<AuthContextType | boolean>(false);
+const AuthContext = createContext<AuthContextType | null>(null);
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth는 LoginProvider 안에서만 사용해야 합니다.');
+  }
   return context;
 };
 
