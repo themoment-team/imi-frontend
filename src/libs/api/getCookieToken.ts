89b24bfa2cb7060865b252405f6975002c0c8145
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-04-22 21:06:01 +0900

    feat:쿠키 가져오기 함수화

---
+++ src/libs/api/getCookieToken.ts
@@ -0,0 +1,5 @@
+export default function getCookie(name: string) {
+  const cookies = document.cookie.split('; ');
+  const cookie = cookies.find((row) => row.startsWith(name + '='));
+  return cookie ? cookie.split('=')[1] : null;
+}
