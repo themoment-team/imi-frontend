c2107e06474285fcaa8e95b0a97c43cd005b813b
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-04-27 22:20:45 +0900

    feat:RefreshResponse 타입 추가

--- src/libs/api/reGetToken.ts
+++ src/libs/api/reGetToken.ts
@@ -1,13 +1,24 @@
+import { getCookie } from '@/utils';
+
 import { axiosInstance } from './axiosInstance';
-import getCookie from './getCookieToken';
+
+type RefreshResponse = {
+  accessToken: string;
+  expiresIn: number;
+  issuedAt: number;
+  refreshToken: string;
+};
 
 export const reGetToken = async () => {
   try {
     const refreshToken = getCookie('refreshToken');
 
-    const response: any = await axiosInstance.post('/auth/refresh', {
-      refreshToken,
-    });
+    const response: RefreshResponse = await axiosInstance.post(
+      '/auth/refresh',
+      {
+        refreshToken,
+      }
+    );
 
     const expireDate = new Date(response.expiresIn).toUTCString();
 
