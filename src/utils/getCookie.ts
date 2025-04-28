65264ae3e54dda547e054cdb5a3214610d3e5b82
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-04-28 16:16:49 +0900

    feat:getCookie 로직 수정

--- src/utils/getCookie.ts
+++ src/utils/getCookie.ts
@@ -1,5 +1,6 @@
 export default function getCookie(name: string): string | undefined {
-  const cookies = document.cookie.split('; ');
-  const cookie = cookies.find((row) => row.startsWith(name + '='));
-  return cookie ? cookie.split('=')[1] : undefined;
+  const cookies = `; ${document.cookie}`;
+  const cookie = cookies.split(`; ${name}=`);
+  if (cookie.length === 2) return cookie.pop()?.split(';').shift();
+  return undefined;
 }
