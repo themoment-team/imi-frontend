606f7425a7b9c1296befac028fa0ed556f1c4975
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-04-28 19:56:11 +0900

    feat:로그인 상태 관리 및 로그아웃 기능 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,11 @@
 import { usePathname, useRouter } from 'next/navigation';
 
 import { ImiLogo } from '@/asset';
+import { useAuth } from '@/libs';
+import { getCookie, isTokenValid } from '@/utils';
 
 import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 import * as S from './header.css';
 
@@ -15,6 +18,8 @@ type NavigationType = {
 
 export default function Header() {
   const router = useRouter();
+
+  const { isLogged, setIsLogged } = useAuth();
 
   const [activePath, setActivePath] = useState<string>('');
 
@@ -37,17 +42,28 @@ export default function Header() {
     setActivePath(pathname);
   }, [pathname]);
 
+  useEffect(() => {
+    const token = getCookie('accessToken');
+    if (token && isTokenValid(token)) {
+      setIsLogged(true);
+    } else {
+      setIsLogged(false);
+    }
+  }, []);
+
   const NavigationArray: NavigationType[] = [
     { name: '동아리', path: '/clubs' },
     { name: '소개서 목록', path: '/profile/list' },
     { name: '자기소개서', path: '/profile/[id]' },
   ];
 
-  /**
-   *  로그인 여부를 판별하기 위해 임시로 만든 변수
-   * 로그인 기능 추가후 삭제 & 변경 바람
-   */
-  const login = true;
+  const handleLogout = () => {
+    document.cookie = 'accessToken=; max-age=-1; path=/;';
+    document.cookie = 'refreshToken=; max-age=-1; path=/;';
+
+    toast.success('로그아웃 되었습니다');
+    setIsLogged(false);
+  };
 
   return (
     display && (
@@ -83,8 +99,10 @@ export default function Header() {
             </div>
           </div>
           <div className={S.LoginSignupBox}>
-            {login ? (
-              <div className={S.Logout}>로그아웃</div>
+            {isLogged ? (
+              <div className={S.Logout} onClick={handleLogout}>
+                로그아웃
+              </div>
             ) : (
               <>
                 <div
