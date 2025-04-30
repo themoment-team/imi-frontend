8ffdf1989b24e90b7574351aaf18e499ce11b69b
h-0y28 <s24015@gsm.hs.kr>
2025-04-30 18:20:48 +0900

    chore: icon name 변경

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -2,7 +2,7 @@ export { default as ImiLogo } from './ImiLogo';
 export { default as Github } from './Github';
 export { default as Insta } from './Insta';
 export { default as Discord } from './Discord';
-export { default as BackBtn } from './BackBtn';
+export { default as ArrowBtn } from './ArrowBtn';
 export { default as OpenEyes } from './OpenEyes';
 export { default as CloseEyes } from './CloseEyes';
 export { default as DownArrow } from './DownArrow';
